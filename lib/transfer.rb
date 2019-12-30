139a727da0341543b84ad21afb290d71d66d61d0
Ayako Wiltshire <awittyl44@gmail.com>
2019-12-30 23:29:36 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -23,7 +23,7 @@ class Transfer
       @receiver.balance += @amount
       @status = "complete"
     
-    else sender.balance < @amount
+    else sender.valid? == false
       @status = "rejected"
       "Transaction rejected. Please check your account balance."
     end
