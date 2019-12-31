3299199224c55200eb7fe0d8d8eb02e0a370e0ce
Ayako Wiltshire <awittyl44@gmail.com>
2019-12-31 00:33:23 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,7 +18,7 @@ class Transfer
   end
   
   def execute_transaction
-    if sender.valid? && receiver.valid? == true
+    while sender.valid? && receiver.valid? == true
       if @sender.balance > @amount && @status == "pending"
         @sender.balance -= @amount
         @receiver.balance += @amount
