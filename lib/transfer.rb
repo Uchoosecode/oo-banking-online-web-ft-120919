eca757806307fe3840148ba2aa5951953c1539d0
Ayako Wiltshire <awittyl44@gmail.com>
2019-12-31 00:45:42 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -23,7 +23,7 @@ class Transfer
       @receiver.balance += @amount
       @status = "complete"
     
-    else @sender.valid? || @receiver.valid? == false 
+    else @sender.valid? || @receiver.valid? == false  
       @sender.close_account
       @status = "rejected"
       "Transaction rejected. Please check your account balance."
